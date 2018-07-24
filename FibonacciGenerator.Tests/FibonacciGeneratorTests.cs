3d96483ca028f4ce0e66ce7ad68febbfac411264
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-24 18:01:27 +0300

    Fibonacci numbers were fixed.

--- FibonacciGenerator.Tests/FibonacciGeneratorTests.cs
+++ FibonacciGenerator.Tests/FibonacciGeneratorTests.cs
@@ -1,8 +1,9 @@
 ﻿namespace FibonacciGenerator.Tests
 {
     using System;
-    using System.Diagnostics;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Numerics;
 
     using NUnit.Framework;
 
@@ -12,27 +13,42 @@
     [TestFixture]
     public class FibonacciGeneratorTests
     {
-        [TestCase(1, new[] { 0 })]
-        [TestCase(2, new[] { 0, 1 })]
-        [TestCase(3, new[] { 0, 1, 1 })]
-        [TestCase(4, new[] { 0, 1, 1, 2 })]
-        [TestCase(5, new[] { 0, 1, 1, 2, 3 })]
-        [TestCase(6, new[] { 0, 1, 1, 2, 3, 5 })]
-        [TestCase(7, new[] { 0, 1, 1, 2, 3, 5, 8 })]
-        [TestCase(8, new[] { 0, 1, 1, 2, 3, 5, 8, 13 })]
-        [TestCase(47, new[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
-                              987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025,
-                              121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578,
-                              5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155,
-                              165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903 })]
-        public void FibonacciGenerator_ValidInput_ValidResult(int amount, int[] expected)
+        [Test]
+        public void FibonacciGenerator_ValidInput_ValidResult1()
         {
-            CollectionAssert.AreEqual(expected, FibonacciGenerator.GenerateSequence(amount).ToArray());
+            BigInteger[] expected =
+            {
+                0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
+                4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229,
+                832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169,
+                63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903,
+                2971215073, 4807526976, 7778742049, 12586269025, 20365011074, 32951280099, 53316291173, 86267571272,
+                139583862445, 225851433717, 365435296162, 591286729879, 956722026041, 1548008755920, 2504730781961, 4052739537881
+            };
+
+            IEnumerable<BigInteger> actual = FibonacciGenerator.GenerateSequence(expected.Length);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FibonacciGenerator_ValidInput_ValidResult2()
+        {
+            BigInteger[] expected =
+            {
+                0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584,
+                4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229,
+                832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169,
+                63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903
+            };
+
+            IEnumerable<BigInteger> actual = FibonacciGenerator.GenerateSequence(expected.Length);
+
+            CollectionAssert.AreEqual(expected, actual);
         }
 
         [TestCase(0)]
         [TestCase(-1)]
-        [TestCase(48)]
         public void FibonacciGenerator_InvalidInput_ThrowsArgumentOutOfRangeExc(int amount)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => FibonacciGenerator.GenerateSequence(amount));
--- FibonacciGenerator/FibonacciGenerator.cs
+++ FibonacciGenerator/FibonacciGenerator.cs
@@ -2,7 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Runtime.CompilerServices;
+    using System.Numerics;
 
     /// <summary>
     /// Class which methods generate Fibonacci sequence.
@@ -21,58 +21,30 @@
         /// <exception cref="ArgumentOutOfRangeException">
         /// Thrown if amount &lt;= 0.
         /// </exception>
-        /// <exception cref="OverflowException">
-        /// Thrown if Fibonacci number exceeds 32 bit signed integer max value.
-        /// </exception>
-        public static IEnumerable<int> GenerateSequence(int amount)
+        public static IEnumerable<BigInteger> GenerateSequence(int amount)
         {
-            int maxFibonacciNumber = GetMaxAmount(sizeof(int));
-            if (amount <= 0 || amount > maxFibonacciNumber)
+            if (amount <= 0)
             {
-                throw new ArgumentOutOfRangeException(
-                    nameof(amount), 
-                    $"Amount of Fibonacci numbers cannot be less or equal 0 or greater than {maxFibonacciNumber}.");
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Fibonacci numbers cannot be less or equal 0.");
             }
 
             return InnerGenerator();
 
-            IEnumerable<int> InnerGenerator()
+            IEnumerable<BigInteger> InnerGenerator()
             {
-                int a = 0;
-                int b = 1;
+                BigInteger a = 0;
+                BigInteger b = 1;
                 while (amount > 0)
                 {
                     yield return a;
 
-                    int temp = a;
+                    BigInteger temp = a;
                     a = b;
                     b = b + temp;
 
                     amount--;
                 }
             }
-        }
-
-        /// <summary>
-        /// Calculates number of the maximum Fibonacci number that can fit into provided size.
-        /// </summary>
-        /// <param name="variableSizeInBytes">
-        /// Size in bytes.
-        /// </param>
-        /// <returns>
-        /// The <see cref="int"/>.
-        /// </returns>
-        private static int GetMaxAmount(int variableSizeInBytes)
-        {
-            // Calculating max Fibonacci number using next relationship:
-            // Fn ~ PHI^n / sqrt(5)
-            const int BITS_PER_BYTE = 8;
-            const double PHI = 1.6180339875; // Golden section.
-
-            double maxValue = Math.Pow(2, (BITS_PER_BYTE * variableSizeInBytes) - 1); // Minus one for sign bit.
-            int n = (int)Math.Log((Math.Sqrt(5) * maxValue) + 0.5, PHI) + 1;
-
-            return n;
         }
     }
 }
